541acd8aa8f9e9e8da144a9933cb4cd32bd57b71
Chris Hague <christopher.a.hague@gmail.com>
2017-02-27 21:02:14 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
+  result = ""
 
+  array.each_with_index do |str, i|
+    if i == array.length - 1
+      result += "#{str}"
+    elsif i == array.length - 2
+      if array.length == 2
+        result += "#{str} and "
+      else
+        result += "#{str}, and "
+      end
+    else
+      result += "#{str}, "
+    end
+  end
+
+  result
 end
